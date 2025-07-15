217c80cd79033e792c40e2d9c6b5f62ec22be442
Vallor23 <enosnyabinda2017@gmail.com>
2025-07-15 10:57:17 +0300

    extend the RequestLog model

--- alx_backend_security/ip_tracking/models.py
+++ alx_backend_security/ip_tracking/models.py
@@ -5,9 +5,14 @@ class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city= models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.ip_address} accessed {self.path} at {self.timestamp}"
     
 class BlockedIP(models.Model): 
     ip_address = models.GenericIPAddressField()
+
+    def __str__(self):
+        return self.ip_address
