a8270846dc62a54c6407205c4cac5b71a5f80d8d
Vallor23 <enosnyabinda2017@gmail.com>
2025-07-15 14:28:19 +0300

    Enhance middleware to include geolocation and caching locatio

--- alx_backend_security/ip_tracking/middleware.py
+++ alx_backend_security/ip_tracking/middleware.py
@@ -1,6 +1,12 @@
+from django.conf import settings
+from django.core.cache import cache
 from django.http import HttpResponseForbidden
 from django.utils.timezone import now
 from .models import RequestLog, BlockedIP
+from django_ip_geolocation.backends import IPGeolocationAPI
+
+API_KEY = settings.IP_GEOLOCATION_KEY
+geo = IPGeolocationAPI(API_KEY)
 
 class LogIPMiddleware:
     def __init__(self, get_response):
@@ -14,11 +20,27 @@ class LogIPMiddleware:
         if BlockedIP.objects.filter(ip_address=ip).exists():
             return HttpResponseForbidden("Access denied: Your IP is blocked.")
         
+        # Get location from cache or API
+        geo_data = cache.get(ip)
+        if not geo_data:
+            try:
+                response = geo.get_geolocation_data(ip)
+                geo_data = {
+                    "country": response.get("country_name"),
+                    "city": response.get("city")
+                }
+                cache.set(ip, geo_data, timeout=60 * 60 * 24)
+            except Exception:
+                geo_data = {"country": None, "city": None}
+
+
         # Log to database
         RequestLog.objects.create(
             ip_address = ip,
             timestamp = now(),
-            path = path
+            path = path,
+            country = geo_data.get("country"),
+            city = geo_data.get("city")
         )
 
         return self.get_response(request)
