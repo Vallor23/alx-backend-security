6da52b47a4c0455c349acba6c526feb529171cf2
Vallor23 <enosnyabinda2017@gmail.com>
2025-07-17 10:58:42 +0300

    create login view

--- alx_backend_security/ip_tracking/views.py
+++ alx_backend_security/ip_tracking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django_ratelimit.decorators import ratelimit
+from django.http import HttpResponse
 # Create your views here.
+
+@csrf_exempt
+@ratelimit(key='user', rate='10/m', method='POST', block=True)
+@ratelimit(key='ip', rate='5/m', method='POST', block=True)
+def login_view(request):
+    return HttpResponse("Login success")
