de16f86c5f33530d42bc249cf74a9c9cd0421366
Vallor23 <enosnyabinda2017@gmail.com>
2025-07-18 18:53:09 +0300

    implement celery task to flag IPs exceeding 100 requests/hour or accessing sensitive paths

---
+++ alx_backend_security/ip_tracking/tasks.py
@@ -0,0 +1,29 @@
+from celery import shared_task
+from django.utils import timezone
+from datetime import timedelta
+
+from .models import SuspiciousIP, RequestLog
+
+SENSITIVE_PATHS = ['/admin', '/login']
+
+@shared_task
+def flag_suspicious_ips():
+    # Get all request from the last hour
+    one_hour_ago = timezone.now() - timedelta(hours=1)
+    recent_logs = RequestLog.objects.filter(timestamp__gte=one_hour_ago)
+
+    # Count requests per IP
+    ip_counts = {}
+    suspicious = {}
+
+    for log in recent_logs:
+        ip = log.ip_address
+        ip_counts[ip] = ip_counts.get(ip, 0) + 1
+
+        if any(log.path.startswith(path) for path in SENSITIVE_PATHS):
+            suspicious[ip] = 'Accessed sensitive path'
+
+    # Flag high request volume
+    for ip, count in ip_counts.items():
+        if count > 100:
+            suspicious[ip] = 'Exceeded 100 requests/hour'
